b84859a73917b49f381308597a799999c4f7efac
Hao.Z <Hao.Z@outlook.de>
2023-06-07 14:36:14 +0200

    fea:add wandb

--- T5_finetuning/train.py
+++ T5_finetuning/train.py
@@ -5,8 +5,19 @@ from transformers import Adafactor, T5ForConditionalGeneration, T5Tokenizer
 from data_utils import DataLoaderCreator
 import argparse
 from tqdm import tqdm
+import wandb
+from dotenv import load_dotenv
+
 
 def main(args):
+
+    # Start a new run
+    load_dotenv()
+    api_key = os.getenv("WANDB_API")
+
+    wandb.login(key=api_key)
+    wandb.init(project='FinalThesis2023', entity='haoz', name='T5_finetuning_csqa')
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Initialize the model and tokenizer
@@ -21,7 +32,10 @@ def main(args):
 
     # Initialize dataloaders using DataLoaderCreator
     data_loader_creator = DataLoaderCreator(source_max_length=args.source_max_length, target_max_length=args.target_max_length, batch_size=args.batch_size)
-    train_dataloader, dev_dataloader, test_dataloader = data_loader_creator.create_dataloaders(data_type='csqa-debug')
+    train_dataloader, dev_dataloader, test_dataloader = data_loader_creator.create_dataloaders(data_type=args.data_type)
+
+    # Watch the model
+    wandb.watch(model, log_freq=100)
 
     # create directory for checkpoints
     ckpt_dir_name = f"checkpoints/bs_{args.batch_size}_opt_{optimizer.__class__.__name__}_epochs_{args.epochs}"
@@ -55,6 +69,9 @@ def main(args):
             # clear the gradients
             optimizer.zero_grad()
 
+            # log the loss
+            wandb.log({"Train Loss": loss.item()})
+
         # Evaluate on dev set
         model.eval()
         total_loss = 0
@@ -71,21 +88,15 @@ def main(args):
                 total_loss += outputs.loss.item()
 
         val_loss = total_loss / len(dev_dataloader)
-        print(f"\nValidation Loss: {val_loss}")
+        print(f"  Validation Loss: {val_loss}")
+        wandb.log({"Validation Loss": val_loss})
 
-        # save the model if it's the best so far
+        # If this model is the best so far, save it
         if val_loss < best_val_loss:
-            print("Saving model...")
             best_val_loss = val_loss
-            new_model_path = os.path.join(ckpt_dir_name, f"model_ep{epoch+1}.pt")
-            torch.save(model.state_dict(), new_model_path)
+            best_model_path = f"{ckpt_dir_name}/model_epoch_{epoch+1}.pt"
+            torch.save(model.state_dict(), best_model_path)
 
-            # remove old best model
-            if best_model_path is not None:
-                os.remove(best_model_path)
-            best_model_path = new_model_path
-
-    # Load the best model
     if best_model_path is not None:
         model.load_state_dict(torch.load(best_model_path))
         print("Loaded best model.")
@@ -106,11 +117,13 @@ def main(args):
             total_loss += outputs.loss.item()
 
     print(f"Test Loss: {total_loss / len(test_dataloader)}")
+    wandb.log({"Test Loss": total_loss / len(test_dataloader)})
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="t5-small")
+    parser.add_argument("--data_type", type=str, default="csqa-debug")
     parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--source_max_length", type=int, default=512)
     parser.add_argument("--target_max_length", type=int, default=128)
